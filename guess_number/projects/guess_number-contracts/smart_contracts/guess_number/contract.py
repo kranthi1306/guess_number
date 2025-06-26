1c1399b7852eca733059c496a5e890ddf5ea31c6
Kranthi Maganti <kranthi.maganti1306@gmail.com>
2025-06-26 15:18:48 +0530

    full code of contract.py

--- guess_number/projects/guess_number-contracts/smart_contracts/guess_number/contract.py
+++ guess_number/projects/guess_number-contracts/smart_contracts/guess_number/contract.py
@@ -1,7 +1,28 @@
-from algopy import *
-class guess_number(ARC4Contract)
-  class global_state(globalState)
-    guess_number=UInt8   
-@Arc4.abimethod()
-def random_number(self,guess:UInt8)->none:
-  
+from algopy import ARC4Contract, UInt64
+from algopy.stdlib import Txn, Global, Address
+from algopy.arc4 import arc4
+class GuessNumber(ARC4Contract):
+    guess_number: UInt64
+    winner: Address
+
+    @Arc4.abimethod()
+    def generated_number(self, hidden_number: UInt64) -> None:
+        assert Txn.sender == Global.creator_address(), "Only creator can set number"
+        assert hidden_number > 0, "Number must be positive"
+        self.guess_number = hidden_number
+        self.winner = Global.zero_address()
+
+    @Arc4.abimethod()
+    def guess(self, user_guess: UInt64) -> str:
+        assert self.winner == Global.zero_address(), "Game already won"
+        if user_guess == self.guess_number:
+            self.winner = Txn.sender
+            return "Correct! You are the winner!"
+        elif user_guess < self.guess_number:
+            return "Too low!"
+        else:
+            return "Too high!"
+
+    @Arc4.abimethod()
+    def get_winner(self) -> Address:
+        return self.winner
