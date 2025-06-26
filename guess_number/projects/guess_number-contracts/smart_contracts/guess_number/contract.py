aab8ba52a0b5d6cda9094a1dc74625a354830931
Kranthi Maganti <kranthi.maganti1306@gmail.com>
2025-06-26 14:02:05 +0530

    modified code

--- guess_number/projects/guess_number-contracts/smart_contracts/guess_number/contract.py
+++ guess_number/projects/guess_number-contracts/smart_contracts/guess_number/contract.py
@@ -1,6 +1,7 @@
 from algopy import *
 class guess_number(ARC4Contract)
-  class local_state(LocalState)
+  class global_state(globalState)
     guess_number=UInt8   
- @Arc4.abimethod()
- def random_number(self,guess:UInt8)->UInt8:
+@Arc4.abimethod()
+def random_number(self,guess:UInt8)->none:
+  
