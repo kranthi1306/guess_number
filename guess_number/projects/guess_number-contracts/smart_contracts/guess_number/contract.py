8eb5d8b6da1acceb292722c229e690fbc95576d2
Kranthi Maganti <kranthi.maganti1306@gmail.com>
2025-06-27 17:31:53 +0530

    Fixed Bugs and Updated Smart Contract with arc4 Standard

--- guess_number/projects/guess_number-contracts/smart_contracts/guess_number/contract.py
+++ guess_number/projects/guess_number-contracts/smart_contracts/guess_number/contract.py
@@ -1,28 +1,27 @@
-from algopy import ARC4Contract, UInt64, Address
-from algopy.stdlib import Txn, Global
-from algopy.arc4 import arc4
+from algopy import ARC4Contract, arc4, Txn, Global
+
 class GuessNumber(ARC4Contract):
-    guess_number: UInt64
-    winner: Address
+    guess_number: arc4.UInt64
+    winner: arc4.Address  # Use Address type for compatibility
 
     @arc4.abimethod()
-    def generated_number(self, hidden_number: UInt64) -> None:
-        assert Txn.sender == Global.creator_address(), "Only creator can set number"
-        assert hidden_number > UInt64(0), "Number must be positive"
+    def generated_number(self, hidden_number: arc4.UInt64) -> None:
+        assert Txn.sender == Global.creator_address, "Only creator can set number"
+        assert hidden_number > arc4.UInt64(0), "Number must be positive"
         self.guess_number = hidden_number
-        self.winner = Global.zero_address()
+        self.winner = arc4.Address(Global.zero_address)  # Properly assign zero address
 
     @arc4.abimethod()
-    def guess(self, user_guess: UInt64) -> str:
-        assert self.winner == Global.zero_address(), "Game already won"
+    def guess(self, user_guess: arc4.UInt64) -> arc4.String:
+        assert self.winner ==arc4.Address(Global.zero_address), "Game already won"
         if user_guess == self.guess_number:
-            self.winner = Txn.sender
-            return "Correct! You are the winner!"
+            self.winner = arc4.Address(Txn.sender)  # Convert sender to Address
+            return arc4.String("Correct! You are the winner!")
         elif user_guess < self.guess_number:
-            return "Too low!"
+            return arc4.String("Too low!")
         else:
-            return "Too high!"
+            return arc4.String("Too high!")
 
     @arc4.abimethod()
-    def get_winner(self) -> Address:
+    def get_winner(self) -> arc4.Address:
         return self.winner
